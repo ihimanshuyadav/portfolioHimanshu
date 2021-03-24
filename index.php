f70d7fe172e6df541be80fcd6cb8b33c0584903c
hiMANshu <60436441+ihimanshuyadav@users.noreply.github.com>
2021-03-25 00:29:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html"); 
+ $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "himanshu@techis.io";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
